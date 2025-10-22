c0c151fea47f1a268d2c3475e35c6d7cd8024e5c
mthus2006 <166153564+mthus2006@users.noreply.github.com>
2025-10-22 15:08:44 -0300

    gg

--- main.js
+++ main.js
@@ -1,9 +1,18 @@
-.const botoes = document.querySelectorAll(".botao");
-const texto = document.querySelectorAll(".abaconteudo");
-for (let i=0;i<botoes.length;i++){
-    botoes[i].onclick = function () {
-    for (let j = 0;j)botoes.length;j++){
-    botoes
-    }
+const botoes = document.querySelectorAll("botao");
+const textos = document.querySelectorAll(".aba-conteudo");
 
+for (let i=0;i <botoes.length;i++){
+    botoes[i].onclick = function (){
+        for(let j=0;j <botoes.length;j++){
+            botoes[j].classList.remove("ativo");
+            textos[j].classList.remove("ativo")
+        }
+    botoes[i].classList.add("ativo");
+    textos[i].classList.add("ativo");
+    }
 }
+]const tempoObjetivo1 = new Date("2023-10-05T00:00:00");
+const tempoObjetivo2 = new Date("2023-12-05T00:00:00");
+const tempoObjetivo3 = new Date("2023-12-30T00:00:00");
+const tempoObjetivo4 = new Date ("2024-02-01T00:00:00");
+const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
